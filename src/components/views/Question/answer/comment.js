39ff0e319b3f889d9310d203518e7d640c548b20
hangchen <hangchen.xie@uzh.ch>
2023-05-21 22:26:59 +0200

    update comment

--- src/components/views/Question/answer/comment.js
+++ src/components/views/Question/answer/comment.js
@@ -2,12 +2,14 @@
 import { List, Avatar } from 'antd';
 
 const CommentListItem = ({ item, actions }) => {
-    console.log('Rendering item with ID: ', item.id);
+    console.log('Rendering item with ID: ', item);
     return (
         <List>
             <List.Item key={item.id} actions={actions}>
                 <List.Item.Meta
-                    avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${item.id}`} />}
+                    avatar={<Avatar
+                        size={54}
+                        src={`https://api.dicebear.com/6.x/adventurer/svg?seed=${item.author_avatar}&scale=90`} />}
                     title={item.author}
                     description={item.content}
                 />
