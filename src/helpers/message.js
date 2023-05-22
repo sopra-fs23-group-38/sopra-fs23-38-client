45d4e9d57c272ad54481f96c97d045e2896138f7
ArthasGAO <91470426+ArthasGAO@users.noreply.github.com>
2023-05-23 01:56:48 +0200

    Delete message.js

--- src/helpers/message.js
+++
@@ -1,9 +0,0 @@
-import requests from "./requests_utils";
-//
-export function insertMessage(data) {
-    return requests.post(`/message/insert?content=${data.content}&fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`);
-}
-//
-export function listMessage(data) {
-    return requests.post(`/message/list?fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`);
-}
