730ab1ec07bbcc62f1e96062ad806a182158a294
ArthasGAO <91470426+ArthasGAO@users.noreply.github.com>
2023-05-23 01:54:28 +0200

    Add files via upload

---
+++ src/helpers/search.js
@@ -0,0 +1,10 @@
+import requests from "./requests_utils";
+
+export function search(params) {
+  return requests.get("/search/", {
+    params: {
+      ...params,
+      searchType: params.searchType,
+    },
+  });
+}
