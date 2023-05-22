4a876ca2b864524fe14142d46378e3e8ffdd71dc
ArthasGAO <91470426+ArthasGAO@users.noreply.github.com>
2023-05-23 01:57:36 +0200

    Delete translator.js

--- src/helpers/translator.js
+++
@@ -1,5 +0,0 @@
-import requests from "./requests_utils";
-
-export function translate(data) {
-    return requests.post(`/translator/?content=${data.content}&targetLanguage=${data.targetLanguage}`);
-}
